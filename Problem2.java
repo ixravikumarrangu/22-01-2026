15d0679b9a245702908005dcedacf45d2a8490d0
ixravikumarrangu <ravikumar.rangu@innovatechs.com>
2026-01-22 14:06:26 +0530

    Adding Problem4.java

--- Problem2.java
+++ Problem2.java
@@ -24,7 +24,7 @@ class Solution{
         return sum;
     }
 }
-class Problem2{
+public class Problem2{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int num=sc.nextInt();
